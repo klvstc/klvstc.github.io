16ce1017533d871dd605fce1f74ceaee9094d804
TANISHQ AGARWAL <44468629+klvstc@users.noreply.github.com>
2020-05-04 00:46:08 +0530

    Update main.js

--- TapIt/main.js
+++ TapIt/main.js
@@ -1,4 +1,4 @@
 if(screen.width>900)
-	{alert("Instructions to Operate: \r\nClick any Key using Keyboard or Click Anywhere on Mouse to see Animation\r\nPlease Turn up your Volume to Max");}
+	{alert("Instructions to Operate: \r\nClick any Key using Keyboard or Click Anywhere on Mouse to see Animation \r\nPlease Turn up your Volume to Max");}
 else
-	{alert("Instructions to Operate: \r\nTap anywhere on the screen to see animation\r\nPlease Turn up your Volume to Max");}
+	{alert("Instructions to Operate: \r\nTap anywhere on the screen to see animation \r\nPlease Turn up your Volume to Max");}
