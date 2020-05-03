3b10974f58d557dc5e5467482ff7e9e2ae425b28
TANISHQ AGARWAL <44468629+klvstc@users.noreply.github.com>
2020-05-04 00:38:57 +0530

    Add files via upload

---
+++ TapIt/main.js
@@ -0,0 +1,4 @@
+if(screen.width>900)
+	{alert("Instructions to Operate: \r\nClick any Key using Keyboard or Click Anywhere on Mouse to see Animation");}
+else
+	{alert("Instructions to Operate: \r\nTap anywhere on the screen to see animation");}
